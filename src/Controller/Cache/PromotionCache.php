8e56964f45134c9ca75d2eff9635377d377ec7fe
Kamil Dlugolecki <kamildlugolecki1998@gmail.com>
2022-09-10 12:30:28 +0200

    Add caching by symfony composer component cache

---
+++ src/Controller/Cache/PromotionCache.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller\Cache;
+
+use App\Entity\Product;
+use App\Repository\PromotionRepository;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
+
+class PromotionCache
+{
+    public function __construct(
+        private CacheInterface $cache,
+        private PromotionRepository $promotionRepository
+    ) {
+    }
+
+    public function findValidForProduct(Product $product, string $requestDate): ?array
+    {
+        $key = sprintf("find-valid-for-product-%d", $product->getId());
+
+       return $this->cache->get($key, function (ItemInterface $item) use ($product, $requestDate){
+            return $this->promotionRepository->findValidForProduct(
+                $product,
+                date_create_immutable($requestDate)
+            );
+        });
+    }
+}
